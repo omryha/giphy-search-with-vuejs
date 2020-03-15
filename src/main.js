1d8a9a31f2a34017d367c2436396513265eeb3c5
omryha <omry.hazut@mail.huji.ac.il>
2020-03-15 10:40:05 +0200

    title mixin removed

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
-import titleMixin from "./titleMixin";
 
 Vue.config.productionTip = false;
-Vue.mixin(titleMixin);
 
 new Vue({
   render: h => h(App)
