3f9c973c2a77a062d0c01c9b5b985c68f71805ac
omryha <omry.hazut@mail.huji.ac.il>
2020-03-16 18:39:32 +0200

    .

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,22 @@
   <div id="app">
     <div class="heading">
       <div class="title">Giphy Search</div>
+      <div class="openingGif-container">
+        <img :src="openingGif" :key="openingGif.id" loading="lazy" />
+      </div>
       <div class="actions">
-        <input type="search" v-on:keyup.enter="getGifs()" v-model="searchTerm" />
-        <select v-model="lang">
+        <input
+          type="search"
+          placeholder="Search a GIF 🐾"
+          v-on:keyup.enter="getGifs()"
+          v-model="searchTerm"
+        />
+        <!--         <select v-model="lang">
+          <option disabled value>Select Language</option>
           <option value="en">English</option>
           <option value="hi">Hebrew</option>
           <option value="ar">Arabic</option>
-        </select>
+        </select>-->
       </div>
       <div class="btn-container">
         <button class="btn-search" @click="getGifs()">Search</button>
@@ -27,8 +36,9 @@ export default {
   title: "Giphy Search",
   data() {
     return {
+      openingGif: "",
       searchTerm: "",
-      lang: "",
+      lang: "en",
       gifs: []
     };
   },
@@ -48,6 +58,23 @@ export default {
         })
         .catch(error => console.log(error));
     },
+    getOpeningGif() {
+      let openingGifUrl =
+        "https://api.giphy.com/v1/gifs/random?api_key=onYkOD8hKuuHkmqEvyINdV1PKXQMjaSk&tag=&rating=G";
+
+      fetch(openingGifUrl)
+        .then(response => {
+          return response.json();
+        })
+        .then(json => {
+          this.buildOpeningGif(json);
+        })
+        .catch(error => console.log(error));
+    },
+    buildOpeningGif(json) {
+      console.log(json.data.id);
+      this.openingGif = `https://media.giphy.com/media/${json.data.id}/giphy.gif`;
+    },
     buildGifs(json) {
       this.gifs = json.data
         .map(gif => gif.id)
@@ -55,6 +82,9 @@ export default {
           return `https://media.giphy.com/media/${gifId}/giphy.gif`;
         });
     }
+  },
+  beforeMount: function() {
+    this.getOpeningGif();
   }
 };
 </script>
@@ -89,6 +119,12 @@ select {
   padding-left: 10px;
   border-radius: 5px;
   border: 1px solid lightsteelblue;
+}
+.openingGif-container {
+  margin-bottom: 30px;
+}
+.openingGif-container img {
+  box-shadow: none;
 }
 .title {
   font-size: 60px;
